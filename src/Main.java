799ef627c5c241c8bddb4b07a0e54aa2a2470fdf
Yauhen Artsiomenka <yauhena@playtika.com>
2023-05-28 01:25:28 +0300

    HomeWork 3.3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //HomeWork3.homeWork3_1();
-        HomeWork3.homeWork3_2();
+        //HomeWork3.homeWork3_2();
+        HomeWork3.homeWork3_3();
 
     }
 }
