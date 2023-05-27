3bfd9ca10506f803774b241708a2ef95ee8bf729
Yauhen Artsiomenka <yauhena@playtika.com>
2023-05-27 22:36:38 +0300

    HomeWork 3.1

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Yauhen";
-        String surname = "Artsiomenka";
-        String profession = "QA engineer";
-        byte var_byte = 127;
-        short var_short = 32767;
-        int var_int = 2147483647;
-        long var_long =  9223372036854775807L;
-        float var_float = 0.1F;
-        double var_double = 0.02;
-        boolean var_boolean = true;
-        char var_char = 111;
-
-        System.out.println(name);
-        System.out.println(surname);
-        System.out.println(profession);
-        System.out.println("byte variable is " + var_byte);
-        System.out.println("short variable is " + var_short);
-        System.out.println("int variable is " + var_int);
-        System.out.println("long variable is " + var_long);
-        System.out.println("float variable is " + var_float);
-        System.out.println("double variable is " + var_double);
-        System.out.println("boolean variable is " + var_boolean);
-        System.out.println("char variable is " + var_char);
-
+        String str = new String();
+        Scanner scanner = new Scanner(System.in);
+        while (!str.equalsIgnoreCase("exit")) {
+            System.out.println("Please enter something");
+            str = scanner.nextLine();
+        }
     }
-
 }
