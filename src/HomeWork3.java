b2cfebecc470096a81f81ba8a8498eb7120bbf04
Yauhen Artsiomenka <yauhena@playtika.com>
2023-05-28 01:24:13 +0300

    HomeWork 3.2

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -10,28 +10,42 @@ public class HomeWork3 {
             str = scanner1.nextLine();
         }
     }
-    public static void homeWork3_2(){
+
+    public static void homeWork3_2() {
 
         Scanner scanner2 = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         int x = scanner2.nextInt();
         int sum = 0;
-        if ( x > 0) {
+        if (x > 0) {
             for (int i = 0; i <= x; i++) {
                 sum += i;
             }
             System.out.printf("The sum of all numbers from 0 to %d is %d", x, sum);
-        }
-            else if (x < 0)  {
+        } else if (x < 0) {
 
-                    for (int j = 0; j >= x; j--) {
-                        sum += j;
-                    }
-            System.out.printf("The sum of all numbers from %d 0 tois %d", x, sum);
+            for (int j = 0; j >= x; j--) {
+                sum += j;
             }
-            else {
-                        System.out.printf("The sum of all numbers from 0 to %d is 0",x);
-                    }
+            System.out.printf("The sum of all numbers from %d  to 0 is %d", x, sum);
+        } else {
+            System.out.printf("The sum of all numbers from 0 to %d is 0", x);
         }
-
     }
+
+    public static void homeWork3_3() {
+        System.out.print("Please enter your score: ");
+        Scanner scanner3 = new Scanner(System.in);
+        int score;
+        do {
+            score = scanner3.nextInt();
+            if (score >= 70 && score <= 100) {
+                System.out.println("Congrats! You’ve passed the test!");
+            } else if (score >= 0 && score <= 100) {
+                System.out.println("Sorry, you’ve failed the test.");
+            } else {
+                System.out.println("Your number is incorrect. Please, enter valid number.");
+            }
+        } while (score < 0 || score > 100);
+    }
+}
