8ddc06306fdfa3306244a5ecdead75b2ddb479ca
Yauhen Artsiomenka <yauhena@playtika.com>
2023-05-29 23:47:13 +0300

    HomeWork 4

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,17 @@ public class Main {
         //HomeWork3.homeWork3_2();
         //HomeWork3.homeWork3_3();
         //HomeWork3.homeWork3_4();
-        HomeWork3.homeWork3_5();
+        //HomeWork3.homeWork3_5();
+
+        EReader kindle = new EReader("Amazon","Kindle Paperwhite",16, 6.8, 300);
+        Book lordOfTheRings = new Book("J.R.R. Tolkien", "Lord of the Rings", "Picture", "Some content" );
+        Book hobbit = new Book("J.R.R. Tolkien", "Hobbit", "Picture2","Some content2");
+
+        String result = lordOfTheRings.getAuthor();
+        System.out.println(result);
+
+
+
 
     }
 }
